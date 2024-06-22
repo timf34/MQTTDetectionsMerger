ea930644de075b6bdcbdb162e737a1d14e5bb991
timf34 <farrelti@tcd.ie>
2024-06-22 17:59:27 +1000

    added optical flow code but it doesn't work very well, although it doesn't raise errors

--- mqtt_listener.py
+++ mqtt_listener.py
@@ -71,14 +71,13 @@ logger.addHandler(file_handler)
 def on_message_received(topic, payload, dup, qos, retain, **kwargs):
     global received_count, received_message, elapsed_time, detections_buffer
 
-    print(f"Received message from topic '{topic}' at {time()}: {payload}")
+    # print(f"Received message from topic '{topic}' at {time()}: {payload}")
 
     received_count += 1
     received_message = payload
 
     end_time = time()
     elapsed_time = end_time - start_time
-    print(f"Received {received_count} messages in {elapsed_time} seconds")
 
     if isinstance(received_message, bytes) and received_message != '':
         received_message = received_message.decode("utf-8")
@@ -101,14 +100,18 @@ def on_message_received(topic, payload, dup, qos, retain, **kwargs):
 
     optical_flow_vector = received_message_json["optical_flow"]
     if optical_flow_vector:
-        flow_vector_buffer[optical_flow_vector["camera_id"]] = np.array(optical_flow_vector["flow_vector"])
+        flow_vector_buffer[optical_flow_vector["camera_id"]] = {"flow_vector": np.array(optical_flow_vector["flow_vector"]), "timestamp": optical_flow_vector["timestamp"]}
 
     if received_count == NUM_MESSAGES:
         received_all_event.set()
 
 
+# TODO: as this is in a thread, errors don't stop things!
 def send_detections_periodically():
+    print("send_detections_periodically started")
+    print(received_all_event.is_set())
     while not received_all_event.is_set():
+        print("made it in")
         current_time = time()
         detections_to_send = []
         vector_flows_to_send: List[Union[Dict[str, np.ndarray], None]] = []
@@ -117,32 +120,22 @@ def send_detections_periodically():
             if current_time - detection.timestamp <= 0.4:  # Check if detection is within 1/4 second + 0.15 seconds for latency between cameras and server (I need to measure the average latency here for this)
                 detections_to_send.append(detection)
 
+        print("after dets buffer loop")
         for camera_id, flow_vector in flow_vector_buffer.items():
-            if current_time - flow_vector["timestamp"] <= 0.4:
-                vector_flows_to_send.append(flow_vector)
+            if flow_vector:
+                if current_time - flow_vector["timestamp"] <= 0.4:
+                    # We need to append the flow vector as {camera_id: flow_vector}
+                    vector_flows_to_send.append({camera_id: flow_vector["flow_vector"]})
 
-        # time_moment = int(time())
-        # print(time_moment)
 
-        # if time_moment % 2 == 0:
-        #     detections_to_send.append(
-        #         Detections(camera_id=1, x=1027, y=844, z=1.0, probability=0.98, timestamp=time())
-        #     )
-        # else:
-        #     detections_to_send.append(
-        #         Detections(camera_id=6, x=964, y=801, z=1.0, probability=0.98, timestamp=time())
-        # )
+                    # vector_flows_to_send.append(flow_vector["flow_vector"])
 
-        # if time_moment % 3 == 0:
-        #     vector_flows_to_send.append(
-        #         {"camera_id": 2, "flow_vector": np.array([0.3, 0.2])}
-        #     )
-        #
-        # print("detections to sends: ", detections_to_send)
-        # print("vector flows to send: ", vector_flows_to_send)
+        print("after flow vector loop")
+        # Temp removal of testing code
 
-        if detections_to_send and vector_flows_to_send:
-
+        print("we in here")
+        if detections_to_send or vector_flows_to_send:
+            print("and in here")
             log_entry = {
                 "type": "detections_to_send",
                 "detections": [d.__dict__ for d in detections_to_send],
@@ -160,11 +153,11 @@ def send_detections_periodically():
                 normalized_y = min(three_d_point.y, 128.8)
 
                 normalized_x = normalized_x / 159.5
-                normalized_x *= 10
+                normalized_x *= 102
 
+                # normalized_y = 128.8 - normalized_y
                 normalized_y = normalized_y / 128.8
                 normalized_y *= 65
-                normalized_y = 65 - normalized_y  # Invert the y-axis
 
                 mqtt_message = {
                   "T": 0,
@@ -201,7 +194,7 @@ if __name__ == "__main__":
         cert_path=config.cert_path,
         client_id="mergerReceiveMessages",
         endpoint=config.endpoint,
-        region="eu-west-1",
+        region="ap-southeast-2",
         priv_key_path=config.private_key_path,
         ca_path=config.root_ca_path
     )
