86628ce86a5f128b7490adddc08ff923e5c96a9f
timf34 <farrelti@tcd.ie>
2024-06-22 18:05:56 +1000

    updated config.py adding a different MQTT topic for AI testing to not confuse hardcoded

--- config.py
+++ config.py
@@ -1,28 +1,33 @@
 import os
 
-USE_OMAR_CERTS: bool = True
+USE_OMAR_CERTS: bool = False
+AUS: bool = True
 
 
 class MQTTMergerConfig:
     def __init__(self):
 
         self.camera_topic: str = "marvel_AUS/cameras"  # MQTT topic to listen where the cameras publish to
-        self.device_topic: str = "marvel_AUS/pub"  # MQTT topic to publish to where the device listens to
+        self.device_topic: str = "marvel_AUS/ai_pub"  # MQTT topic to publish to where the device listens to
 
         if os.name == 'nt':
-            self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.eu-west-1.amazonaws.com"
+            self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.ap-southeast-2.amazonaws.com"
+            self.cert_path: str = "./certs/marvel-fov-test-sydney/certificate.pem.crt"
+            self.private_key_path: str = "./certs/marvel-fov-test-sydney/private.pem.key"
+            self.root_ca_path: str = "./certs/marvel-fov-test-sydney/AmazonRootCA1.pem"
+
+            # self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.eu-west-1.amazonaws.com"
             # self.cert_path: str = "./certs/marvel-fov-test/certificate.pem.crt"
             # self.private_key_path: str = "./certs/marvel-fov-test/private.pem.key"
             # self.root_ca_path: str = "./certs/marvel-fov-test/AmazonRootCA1.pem"
 
             # Omars
             # TODO: temp test
-            self.endpoint: str = "a13d7wu4wem7v1-ats.iot.eu-west-1.amazonaws.com"
-            self.cert_path: str = "./certs/omar-certs-for-testing-old-firmware/certificate.pem.crt"
-            self.private_key_path: str = "./certs/omar-certs-for-testing-old-firmware/private.pem.key"
-            self.root_ca_path: str = "./certs/omar-certs-for-testing-old-firmware/root.pem"
+            # self.endpoint: str = "a13d7wu4wem7v1-ats.iot.ap-southeast-2.amazonaws.com"
+            # self.cert_path: str = "./certs/omar-certs-for-testing-old-firmware/certificate.pem.crt"
+            # self.private_key_path: str = "./certs/omar-certs-for-testing-old-firmware/private.pem.key"
+            # self.root_ca_path: str = "./certs/omar-certs-for-testing-old-firmware/root.pem"
         else:
-
             if not USE_OMAR_CERTS:
                 self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.ap-southeast-2.amazonaws.com"
                 self.cert_path: str = "./certs/marvel-fov-test-sydney/certificate.pem.crt"
